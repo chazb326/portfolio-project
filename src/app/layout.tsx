9037ab20b6a1b2a19549c32c75e19dbe96bff3ef
Charlie Burian <charlie.burian@galileo-insights.com>
2023-12-21 09:42:03 -0600

    under construction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import styles from './page.module.css'
 
 export const metadata: Metadata = {
   title: "Charlie's React App"
@@ -12,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={styles.columnBox} style={{zIndex: "100", width: "100vw", height: "100vh", backgroundColor: "lightgray", margin: "0 auto"}}>
+          <h1>Under Construction!!</h1>
+        </div>
+        
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>
-        <ul className={styles.rowBox}>
-          <li>Link 1</li>
-          <li>Link 2</li>
-          <li>Link 3</li>
-        </ul>
-      </header>
-      <div className={styles.columnBox}>
-        <h1>Charlie&apos;s App</h1>
-        <h2>About Me</h2>
-        <h2>Employment</h2>
-        <h2>What I&apos;m Working On</h2>
+      
+      <div className={styles.pageContent} style={{}}>
+        
       </div>
     </main>
   )
